cc542b8d1b9093550423a2cd09140fd3aed4c647
mk-repository <abdulmalikalawode@gmail.com>
2026-06-01 11:10:17 +0000

    add deleteProduct function

--- controllers/productController.js
+++ controllers/productController.js
@@ -77,5 +77,19 @@ const getProductById = async (req, res) => {
   }
 };
 
+const deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      await product.deleteOne();
+      res.json({ message: "Product deleted successfully" });
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = { createProduct, updateProduct, getProducts, getProductById, deleteProduct };
 
