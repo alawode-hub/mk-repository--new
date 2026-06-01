7747f0be1eff3e28a7a61eaea0ab61da3a29a396
mk-repository <abdulmalikalawode@gmail.com>
2026-06-01 11:07:24 +0000

    add getProducts function

--- controllers/productController.js
+++ controllers/productController.js
@@ -55,4 +55,27 @@ const updateProduct = async (req, res) => {
   }
 };
 
+const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = { createProduct, updateProduct, getProducts, getProductById, deleteProduct };
+
